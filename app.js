ab70a160ece5a9dbcad4e8f8c98aa935276ab5d6
Kesline Diogene <deka@keslines-air.ssw.umb.edu>
2024-03-25 15:50:29 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// this is a variable that is set to my age
+const myAge = 22;
+//variable will change
+let earlyYears = 2;
+earlyYears *= 10.5;
+//subtracting from my age
+laterYears = myAge -2;
+//console.log(laterYears);
+//mutliplication
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+//addition
+myAgeInDogYears = earlyYears + laterYears;
+//call it in lower case
+myName = 'Kesline';
+myName = myName.toLowerCase();
+console.log('My name is ' + myName + '.' + ' I am '+ myAge + ' years old in human years which is ' + myAgeInDogYears +' years old in dog years.' )
